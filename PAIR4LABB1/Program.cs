40906285af62e1c5ba5b21ac6fe83fafde761345
emilgrupp16 <emil.johansson62@hotmail.com>
2024-11-25 13:49:30 +0100

    Kommentarer

--- PAIR4LABB1/Program.cs
+++ PAIR4LABB1/Program.cs
@@ -8,7 +8,7 @@ namespace PAIR4LABB1
         static void Main(string[] args)
 
         {
-
+            //Först skriver vi ut meddelande till användaren om valen hen har.
             Console.WriteLine("****************************************************************\n");
             Console.WriteLine("Välkommen till flygtidsberäknaren!\n");
 
@@ -19,21 +19,24 @@ namespace PAIR4LABB1
 
             Console.WriteLine($"Skriv ditt val här:");
 
+            //Här läser vi in användarens input och gör om det till en "int"
             string userInput = Console.ReadLine();
 
             int choice = Int32.Parse(userInput);
 
+            //Sen skapar vi samt delar upp timmar och minuter i variabler av typen "int"
             int travelHour = 7;
             int travelMinutes1 = 25;
             int timeDifference = 6;
 
             int departureTimeHour1 = 14;
             int departureTimeMinute1 = 3;
+            //Här konverterar vi "departureTimeMinute1" till en string med "ToString("00") så att vi får två decimaler i utskriften.
             string departureTimeMinute = departureTimeMinute1.ToString("00");
             int departureTimeHour2 = 10;
             int departureTimeMinute2 = 10;
 
-
+            //Här gör vi våra uträkningar för att få fram rätt tider åt användaren.
             string departureTime1 = ($"{departureTimeHour1}:{departureTimeMinute}");
             string arrivalTime1 = ($"{departureTimeHour1 + travelHour - timeDifference}:{departureTimeMinute1 + travelMinutes1}");
 
@@ -43,6 +46,8 @@ namespace PAIR4LABB1
 
             Console.WriteLine("**********************************************************************\n");
 
+
+            //Här använder vi tre olika "if-satser" för att skriva ut rätt tider beroende på vad användaren gör för val.
             if (choice == 1)
             {
 
@@ -69,6 +74,7 @@ namespace PAIR4LABB1
 
             Console.WriteLine("\n**************************************************************************");
 
+            //Till sist så skriver vi ut ett meddelande till användern som deklarerar att programmet är klart.
             Console.WriteLine("\nTack för att du använde Flygtidsberäknaren! Nu stängs programmet ner.");
 
 
